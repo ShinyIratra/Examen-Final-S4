424376eb7c439e03f8b66d07f66608f6a3abbc92
ShinyIratra <Iratra53@gmail.com>
2025-07-07 11:50:51 +0300

    feat : generaliser CRUD fetch

--- ws/index.php
+++ ws/index.php
@@ -1,6 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 require 'db.php';
+require 'RequeteBase.php';
 
 // Middleware pour CORS sur toutes les réponses
 Flight::before('start', function() {
@@ -20,40 +21,11 @@ Flight::before('start', function() {
     }
 });
 
-Flight::route('GET /etudiants', function() {
-    $db = getDB();
-    $stmt = $db->query("SELECT * FROM etudiant");
-    Flight::json($stmt->fetchAll(PDO::FETCH_ASSOC));
-});
-
-Flight::route('GET /etudiants/@id', function($id) {
-    $db = getDB();
-    $stmt = $db->prepare("SELECT * FROM etudiant WHERE id = ?");
-    $stmt->execute([$id]);
-    Flight::json($stmt->fetch(PDO::FETCH_ASSOC));
-});
-
-Flight::route('POST /etudiants', function() {
-    $data = Flight::request()->data;
-    $db = getDB();
-    $stmt = $db->prepare("INSERT INTO etudiant (nom, prenom, email, age) VALUES (?, ?, ?, ?)");
-    $stmt->execute([$data->nom, $data->prenom, $data->email, $data->age]);
-    Flight::json(['message' => 'Étudiant ajouté', 'id' => $db->lastInsertId()]);
-});
-
-Flight::route('PUT /etudiants/@id', function($id) {
-    $data = Flight::request()->data;
-    $db = getDB();
-    $stmt = $db->prepare("UPDATE etudiant SET nom = ?, prenom = ?, email = ?, age = ? WHERE id = ?");
-    $stmt->execute([$data->nom, $data->prenom, $data->email, $data->age, $id]);
-    Flight::json(['message' => 'Étudiant modifié']);
-});
-
-Flight::route('DELETE /etudiants/@id', function($id) {
-    $db = getDB();
-    $stmt = $db->prepare("DELETE FROM etudiant WHERE id = ?");
-    $stmt->execute([$id]);
-    Flight::json(['message' => 'Étudiant supprimé']);
-});
+// Utilise les fonctions génériques pour CRUD étudiant
+getAll('etudiants', 'etudiant');
+getById('etudiants', 'etudiant', 'id');
+insert('etudiants', 'etudiant', ['nom' => '', 'prenom' => '', 'email' => '', 'age' => 0]);
+update('etudiants', 'etudiant', ['nom' => '', 'prenom' => '', 'email' => '', 'age' => 0], 'id');
+delete('etudiants', 'etudiant', 'id');
 
 Flight::start();
